467ff10654fa9a1599090085b6be434da5e4d42b
Fran <franbermejo.dam@gmail.com>
2025-02-08 21:35:09 +0100

    Proyecto creado con las dependencias

---
+++ Portfolio2/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+        <footer className="bg-gray-200 p-4 text-center">
+            <p className="text-sm text-gray-700">© 2023 Mi Portafolio. Todos los derechos reservados.</p>
+        </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ Portfolio2/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-gray-200 p-4 text-center align-middle'>
+            <h1 className="text-3xl font-bold text-center">Header </h1>
+        </div>
+    );
+}
+
+export default Header;
