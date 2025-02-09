a3fe08383134442cabfb10e6b8a22fde00950297
ignaciodiazjimenez <ignaciodeloyoladiazjimenez@gmail.com>
2025-02-10 00:18:31 +0100

    Segundo Commit Menu Desplegable con estilo

--- Portfolio2/src/components/Header.jsx
+++ Portfolio2/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-[#0a353d] text-[#e6ff2b] p-4 text-center align-middle'>
+        <div className='bg-[#0a353d] text-[#e6ff2b] p-1 text-center align-middle'>
             <h1 className="text-3xl font-bold text-center"> Mi Porfolio🧐! </h1>
         </div>
     );
